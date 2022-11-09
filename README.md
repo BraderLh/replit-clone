9dc0eed700c125b059ca02ac0a9ab897066fbddd
BraderLh <blipe@unsa.edu.pe>
2022-11-09 02:15:49 -0500

    Frontend completed

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# Replit clone
+# Python online compiler
+
+## Frontend 
+
+### Installation
+
+For making a frontend container just run these comands in your command line:
+
+1. Find and change your application and Docker file path using command line (Windows/Linux or Mac): 
+> `$ cd \path\to\app`
+2.  Make sure Docker and source files exits navigatin usind *cd* or *dir*.
+3. Then exectute these commands:
+>`$ docker build -t <docker-image-name> .`
+
+>`$ docker run -dit --name <container-name> -p 8080:80 <docker-inage-name>`
+4. Finally, visit the [Apache HTTP Server](http://localhost:8080/) and you will see It works!
+
+
+
+
 
---
+++ frontend/src/js/editor.js
@@ -0,0 +1,91 @@
+// Retrieve Elements
+const consoleLogList = document.querySelector('.editor__console-logs');
+const executeCodeBtn = document.querySelector('.editor__run');
+const resetCodeBtn = document.querySelector('.editor__reset');
+
+// Setup Ace
+let codeEditor = ace.edit("editorCode");
+let defaultCode = 'Escribe tu codigo aqui';
+let consoleMessages = [];
+
+let editorLib = {
+    clearConsoleScreen() {
+        consoleMessages.length = 0;
+
+        // Remove all elements in the log list
+        while (consoleLogList.firstChild) {
+            consoleLogList.removeChild(consoleLogList.firstChild);
+        }
+    },
+    printToConsole() {
+        consoleMessages.forEach(log => {
+            const newLogItem = document.createElement('li');
+            const newLogText = document.createElement('pre');
+
+            newLogText.className = log.class;
+            newLogText.textContent = `> ${log.message}`;
+
+            newLogItem.appendChild(newLogText);
+
+            consoleLogList.appendChild(newLogItem);
+        })
+    },
+    init() {
+        // Configurar Ace
+
+        // Theme
+        codeEditor.setTheme("ace/theme/github");
+
+        // Set language
+        codeEditor.session.setMode("ace/mode/python");
+
+        // Set Options
+        codeEditor.setOptions({
+            fontFamily: 'Inconsolata',
+            fontSize: '12pt',
+            hScrollBarAlwaysVisible: true,
+            vScrollBarAlwaysVisible: true,
+            enableBasicAutocompletion: true,
+            enableLiveAutocompletion: true,
+        });
+
+
+        // Set Default Code
+        codeEditor.setValue(defaultCode);
+        
+        
+    }
+}
+
+// Events
+executeCodeBtn.addEventListener('click', () => {
+    editorLib.clearConsoleScreen();
+
+    // Obtener el código que digitó el usuario
+    const userCode = codeEditor.getValue();
+    $.ajax({ 
+        crossDomain: true,
+        type:"POST", 
+        url:"http://34.134.205.13:8080/exec", 
+        data:JSON.stringify({code:userCode}),
+        success:function(datos){
+            consoleMessages.push({message:datos.result,class:"error"})
+            console.log(datos,consoleMessages)
+            editorLib.printToConsole()        
+        },
+        contentType:"application/json",
+        dataType: "json"
+
+    })
+    editorLib.printToConsole();
+});
+
+resetCodeBtn.addEventListener('click', () => {
+    // Clear ace editor
+    codeEditor.setValue(defaultCode);
+
+    // Clear console messages
+    editorLib.clearConsoleScreen();
+})
+
+editorLib.init();
